a22b325428fff97c7344463c2e16f341e7463fe9
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2020-09-08 11:45:47 +0200

    use better zip filename pattern

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ set -eu                # Always put this in Bourne shell scripts
 IFS=$(printf '\n\t')   # Always put this in Bourne shell scripts
 mkdir -p /data/downloads
 ./freshen_local_nasr.sh /data/downloads && \
-  FILE=$(ls /data/downloads) && \
-  ./create_databases.sh "/data/downloads/$FILE" && \
+  FILE=$(ls /data/downloads/28DaySubscription_Effective_*.zip) && \
+  ./create_databases.sh "$FILE" && \
   cp *.sqlite /data
